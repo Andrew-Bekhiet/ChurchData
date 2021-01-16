dbe18a3c18963e41f3f9b3893c930d63a37215ff
Andrew Bekhiet <andrewbekhiet@gmail.com>
2021-01-16 20:01:09 +0200

    Allow User to edit profile picture Fix Icon Shadow Optimize Expandable Appbar title

--- lib/views/ui/MyAccount.dart
+++ lib/views/ui/MyAccount.dart
@@ -5,6 +5,9 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart'
     if (dart.library.html) 'package:churchdata/FirebaseWeb.dart' hide User;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 import '../../EncryptionKeys.dart';
@@ -35,32 +38,126 @@ class _MyAccountState extends State<MyAccount> {
           return NestedScrollView(
             headerSliverBuilder: (context, _) => <Widget>[
               SliverAppBar(
-                  actions: [
-                    IconButton(
-                      icon: Icon(Icons.info_outline),
-                      onPressed: () async {
-                        var person = await user.getPerson();
-                        if (person == null) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('لم يتم إيجاد بيانات للمستخدم'),
-                              duration: Duration(seconds: 3),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.info_outline),
+                    onPressed: () async {
+                      var person = await user.getPerson();
+                      if (person == null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('لم يتم إيجاد بيانات للمستخدم'),
+                            duration: Duration(seconds: 3),
+                          ),
+                        );
+                        return;
+                      }
+                      personTap(await user.getPerson(), context);
+                    },
+                    tooltip: 'عرض بياناتي داخل البرنامج',
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      var source = await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          actions: <Widget>[
+                            TextButton.icon(
+                              onPressed: () => Navigator.of(context).pop(true),
+                              icon: Icon(Icons.camera),
+                              label: Text('التقاط صورة من الكاميرا'),
                             ),
-                          );
-                          return;
-                        }
-                        personTap(await user.getPerson(), context);
-                      },
-                      tooltip: 'عرض بياناتي داخل البرنامج',
+                            TextButton.icon(
+                              onPressed: () => Navigator.of(context).pop(false),
+                              icon: Icon(Icons.photo_library),
+                              label: Text('اختيار من المعرض'),
+                            ),
+                            TextButton.icon(
+                              onPressed: () =>
+                                  Navigator.of(context).pop('delete'),
+                              icon: Icon(Icons.delete),
+                              label: Text('حذف الصورة'),
+                            ),
+                          ],
+                        ),
+                      );
+                      if (source == null) return;
+                      if (source == 'delete') {
+                        //TODO: showdialog
+                        return;
+                      }
+                      if ((source &&
+                              !(await Permission.storage.request())
+                                  .isGranted) ||
+                          !(await Permission.camera.request()).isGranted)
+                        return;
+                      var selectedImage = (await ImagePicker().getImage(
+                          source: source
+                              ? ImageSource.camera
+                              : ImageSource.gallery));
+                      if (selectedImage == null) return;
+                      var finalImage = (await ImageCropper.cropImage(
+                          sourcePath: selectedImage.path,
+                          cropStyle: CropStyle.circle,
+                          androidUiSettings: AndroidUiSettings(
+                              toolbarTitle: 'قص الصورة',
+                              toolbarColor: Theme.of(context).primaryColor,
+                              toolbarWidgetColor: Theme.of(context)
+                                  .primaryTextTheme
+                                  .headline6
+                                  .color,
+                              initAspectRatio: CropAspectRatioPreset.original,
+                              lockAspectRatio: false)));
+                      if (await showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              content: Text('هل تريد تغير الصورة؟'),
+                              actions: [
+                                TextButton(
+                                  child: Text('تغيير'),
+                                  onPressed: () => Navigator.pop(context, true),
+                                ),
+                                TextButton(
+                                  child: Text('تراجع'),
+                                  onPressed: () =>
+                                      Navigator.pop(context, false),
+                                )
+                              ],
+                            ),
+                          ) ??
+                          false) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('جار التحميل'),
+                          duration: Duration(minutes: 2),
+                        ));
+                        await user.photoRef.putFile(finalImage);
+                        user.reloadImage();
+                        setState(() {});
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('تم بنجاح'),
+                        ));
+                      }
+                    },
+                    icon: Icon(Icons.photo_camera),
+                  ),
+                ],
+                expandedHeight: 250.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: LayoutBuilder(
+                  builder: (context, constraints) => FlexibleSpaceBar(
+                    title: AnimatedOpacity(
+                      duration: Duration(milliseconds: 300),
+                      opacity: constraints.biggest.height > kToolbarHeight * 1.7
+                          ? 0
+                          : 1,
+                      child: Text(user.name),
                     ),
-                  ],
-                  expandedHeight: 250.0,
-                  floating: false,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    title: Text(user.name),
                     background: user.getPhoto(false, false),
-                  )),
+                  ),
+                ),
+              ),
             ],
             body: ListView(
               children: <Widget>[
