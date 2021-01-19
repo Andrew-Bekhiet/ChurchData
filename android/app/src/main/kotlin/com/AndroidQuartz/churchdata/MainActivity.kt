f49529050af761514d2a20294767151e177cb920
Andrew Bekhiet <andrewbekhiet@gmail.com>
2021-01-19 21:05:35 +0200

    Clean MainActivity.kt dependencies

--- android/app/src/main/kotlin/com/AndroidQuartz/churchdata/MainActivity.kt
+++ android/app/src/main/kotlin/com/AndroidQuartz/churchdata/MainActivity.kt
@@ -1,43 +1,17 @@
 package com.AndroidQuartz.churchdata
-
-import android.app.AlarmManager
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkInfo
-import android.os.Build
-import android.util.Base64
-import android.util.Log
 import android.view.SurfaceView
 import android.view.WindowManager
 import androidx.annotation.NonNull
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.google.firebase.Timestamp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Source
-import com.it_nomads.fluttersecurestorage.ciphers.StorageCipher18Implementation
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-import java.nio.charset.Charset
-import java.util.*
 
 class MainActivity: FlutterFragmentActivity() {
-
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
         SurfaceView(applicationContext).setSecure(true)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
     }
-
 }
