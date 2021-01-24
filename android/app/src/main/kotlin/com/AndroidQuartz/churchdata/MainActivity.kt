d8b2659f6813d895c2505cae598cd938c9ddd20f
Andrew Bekhiet <andrewbekhiet@gmail.com>
2021-01-24 20:29:54 +0200

    Updated Firebase Messaging

--- android/app/src/main/kotlin/com/AndroidQuartz/churchdata/MainActivity.kt
+++ android/app/src/main/kotlin/com/AndroidQuartz/churchdata/MainActivity.kt
@@ -4,12 +4,11 @@ import android.view.WindowManager
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.plugins.util.GeneratedPluginRegister
 
 class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        GeneratedPluginRegister.registerGeneratedPlugins(flutterEngine)
         SurfaceView(applicationContext).setSecure(true)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,6 +9,7 @@ import 'package:file_picker/src/file_picker_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
@@ -28,6 +29,7 @@ void registerPlugins(Registrar registrar) {
   FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
   GoogleMapsPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
