ece4db26b885d37b8e367a8966c61998361b7b52
Andrew Bekhiet <andrewbekhiet@gmail.com>
2021-01-24 20:39:06 +0200

    Updated Notifications implementation

--- lib/views/ui/NotificationsPage.dart
+++ lib/views/ui/NotificationsPage.dart
@@ -13,40 +13,61 @@ class NotificationsPage extends StatefulWidget {
 
 class _NotificationsPageState extends State<NotificationsPage> {
   @override
+  void dispose() {
+    Hive.box<Map>('Notifications').close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('الإشعارات'),
       ),
-      body: ListView.builder(
-        itemCount: Hive.box<Map<dynamic, dynamic>>('Notifications').length,
-        itemBuilder: (context, i) {
-          return n.Notification.fromMessage(
-            Hive.box<Map<dynamic, dynamic>>('Notifications')
-                .getAt(i)
-                .cast<String, dynamic>(),
-            () async {
-              if (await showDialog(
-                    context: context,
-                    builder: (context) => DataDialog(
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.of(context).pop(true),
-                          child: Text('نعم'),
-                        )
-                      ],
-                      content: Text('هل تريد حذف هذا الاشعار؟'),
-                    ),
-                  ) ==
-                  true) {
-                await Hive.box<Map<dynamic, dynamic>>('Notifications')
-                    .deleteAt(i);
-                setState(() {});
-              }
-            },
-          );
-        },
-      ),
+      body: FutureBuilder(
+          future: Hive.openBox<Map>('Notifications'),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done)
+              return Center(child: const CircularProgressIndicator());
+            return ListView.builder(
+              itemCount:
+                  Hive.box<Map<dynamic, dynamic>>('Notifications').length,
+              itemBuilder: (context, i) {
+                return n.Notification.fromMessage(
+                  Hive.box<Map<dynamic, dynamic>>('Notifications')
+                      .getAt(Hive.box<Map<dynamic, dynamic>>('Notifications')
+                              .length -
+                          i -
+                          1)
+                      .cast<String, dynamic>(),
+                  () async {
+                    if (await showDialog(
+                          context: context,
+                          builder: (context) => DataDialog(
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(true),
+                                child: Text('نعم'),
+                              )
+                            ],
+                            content: Text('هل تريد حذف هذا الاشعار؟'),
+                          ),
+                        ) ==
+                        true) {
+                      await Hive.box<Map<dynamic, dynamic>>('Notifications')
+                          .deleteAt(
+                              Hive.box<Map<dynamic, dynamic>>('Notifications')
+                                      .length -
+                                  i -
+                                  1);
+                      setState(() {});
+                    }
+                  },
+                );
+              },
+            );
+          }),
     );
   }
 }
