752ae7a5bd7287c9927952c6dd303dc243bf2608
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2021-07-20 20:16:04 +0200

    fix: incorrect error handling logic

--- lib/Models/loading_widget.dart
+++ lib/Models/loading_widget.dart
@@ -51,13 +51,13 @@ class Loading extends StatelessWidget {
                       label: Text('اضغط لمزيد من المعلومات'),
                       icon: Icon(Icons.error),
                       onPressed: () {
-                        if (message != 'Exception: Error Update User Data')
-                          showErrorDialog(context, message);
-                        else if (message !=
+                        if (message == 'Exception: Error Update User Data')
+                          showErrorUpdateDataDialog(context: context);
+                        else if (message ==
                             'Exception: يجب التحديث لأخر إصدار لتشغيل البرنامج')
                           Updates.showUpdateDialog(context, canCancel: false);
                         else
-                          showErrorUpdateDataDialog(context: context);
+                          showErrorDialog(context, message);
                       },
                     )
                   else
